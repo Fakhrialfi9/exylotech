d0d604658e618d22b8f2112ccc9f3f1fc1a2ff79
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2024-06-10 01:45:11 +0700

    add : build

--- src/Api/MainApi.jsx
+++ src/Api/MainApi.jsx
@@ -1,88 +1,3 @@
-import axios from "axios";
-
-const baseURL = "http://localhost:3001";
-const api = axios.create({
-  baseURL: baseURL,
-});
-
-export const getAllProducts = async () => {
-  try {
-    const response = await axios({
-      url: `${baseURL}/product`,
-      responseType: "json",
-      credentials: "include",
-      mode: "no-cors",
-      headers: {
-        Accept: "application/json; odata=verbose",
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching products", error);
-    throw error;
-  }
-};
-
-export const getAllDate = async (startDate, endDate) => {
-  try {
-    const response = await axios({
-      url: `${baseURL}/date`,
-      responseType: "json",
-      credentials: "include",
-      mode: "no-cors",
-      headers: {
-        Accept: "application/json; odata=verbose",
-      },
-      params: {
-        start_date: startDate,
-        end_date: endDate,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching sales by date range", error);
-    throw error;
-  }
-};
-
-export const getAllSales = async () => {
-  try {
-    const response = await axios({
-      url: `${baseURL}/sales`,
-      responseType: "json",
-      credentials: "include",
-      mode: "no-cors",
-      headers: {
-        Accept: "application/json; odata=verbose",
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching sales", error);
-    throw error;
-  }
-};
-
-export const getAllRevenue = async () => {
-  try {
-    const response = await axios({
-      url: `${baseURL}/revenue`,
-      responseType: "json",
-      credentials: "include",
-      mode: "no-cors",
-      headers: {
-        Accept: "application/json; odata=verbose",
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching revenue", error);
-    throw error;
-  }
-};
-
-export default api;
-
 // import axios from "axios";
 
 // const baseURL = "http://localhost:3001";
@@ -90,19 +5,17 @@ export default api;
 //   baseURL: baseURL,
 // });
 
-// export const getAllSales = async () => {
-//   try {
-//     const response = await api.get("/sales");
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error fetching data", error);
-//     throw error;
-//   }
-// };
-
 // export const getAllProducts = async () => {
 //   try {
-//     const response = await api.get("/products");
+//     const response = await axios({
+//       url: `${baseURL}/product`,
+//       responseType: "json",
+//       credentials: "include",
+//       mode: "no-cors",
+//       headers: {
+//         Accept: "application/json; odata=verbose",
+//       },
+//     });
 //     return response.data;
 //   } catch (error) {
 //     console.error("Error fetching products", error);
@@ -110,9 +23,16 @@ export default api;
 //   }
 // };
 
-// export const getSalesByDateRange = async (startDate, endDate) => {
+// export const getAllDate = async (startDate, endDate) => {
 //   try {
-//     const response = await api.get("/sales", {
+//     const response = await axios({
+//       url: `${baseURL}/date`,
+//       responseType: "json",
+//       credentials: "include",
+//       mode: "no-cors",
+//       headers: {
+//         Accept: "application/json; odata=verbose",
+//       },
 //       params: {
 //         start_date: startDate,
 //         end_date: endDate,
@@ -125,4 +45,84 @@ export default api;
 //   }
 // };
 
+// export const getAllSales = async () => {
+//   try {
+//     const response = await axios({
+//       url: `${baseURL}/sales`,
+//       responseType: "json",
+//       credentials: "include",
+//       mode: "no-cors",
+//       headers: {
+//         Accept: "application/json; odata=verbose",
+//       },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching sales", error);
+//     throw error;
+//   }
+// };
+
+// export const getAllRevenue = async () => {
+//   try {
+//     const response = await axios({
+//       url: `${baseURL}/revenue`,
+//       responseType: "json",
+//       credentials: "include",
+//       mode: "no-cors",
+//       headers: {
+//         Accept: "application/json; odata=verbose",
+//       },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching revenue", error);
+//     throw error;
+//   }
+// };
+
 // export default api;
+
+// // import axios from "axios";
+
+// // const baseURL = "http://localhost:3001";
+// // const api = axios.create({
+// //   baseURL: baseURL,
+// // });
+
+// // export const getAllSales = async () => {
+// //   try {
+// //     const response = await api.get("/sales");
+// //     return response.data;
+// //   } catch (error) {
+// //     console.error("Error fetching data", error);
+// //     throw error;
+// //   }
+// // };
+
+// // export const getAllProducts = async () => {
+// //   try {
+// //     const response = await api.get("/products");
+// //     return response.data;
+// //   } catch (error) {
+// //     console.error("Error fetching products", error);
+// //     throw error;
+// //   }
+// // };
+
+// // export const getSalesByDateRange = async (startDate, endDate) => {
+// //   try {
+// //     const response = await api.get("/sales", {
+// //       params: {
+// //         start_date: startDate,
+// //         end_date: endDate,
+// //       },
+// //     });
+// //     return response.data;
+// //   } catch (error) {
+// //     console.error("Error fetching sales by date range", error);
+// //     throw error;
+// //   }
+// // };
+
+// // export default api;
